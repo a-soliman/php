3ee15d3ecd97a16f795b2b0897163976fcc702fb
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-11 20:43:09 -0800

    practicing php function

---
+++ my-function.php
@@ -0,0 +1,9 @@
+<?php 
+
+/* Definiong first php function */
+
+function myfirstfunction() {
+	echo "PHP is an easy yet powerful langauge";
+}
+
+?>
--- php-variable.php
+++ php-variable.php
@@ -3,4 +3,5 @@
 $newyork = "I Love Big Apple!";
 $abc = 75;
 $xyz = 10.9;
+
 ?>
