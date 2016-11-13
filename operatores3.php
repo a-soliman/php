fe8d603417d855bc1e9a25a33685e2fcbd3e5235
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-12 21:42:57 -0800

    more on operatores

--- operatores3.php
+++ operatores3.php
@@ -1,13 +1,16 @@
 <?php 
 
-$x = 10;
+$x = 20;
 $y = 5;
 $name = "Ahmed";
 
 /* Arithmetic Operators */
 
-echo $x + $y;
-echo $x - $y;
-echo $x * $y;
-echo $x / $y;
+// echo $x + $y;
+// echo $x - $y;
+// echo $x * $y;
+// echo $x / $y;
+// echo $x % $y;
+
+
 ?>
