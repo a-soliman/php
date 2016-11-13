8990cfbd0323653c0f010a8229a5741647dcb3ae
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-12 20:58:15 -0800

    practicing functions with pram

---
+++ functionWithPram.php
@@ -0,0 +1,19 @@
+<html>
+	
+	<head>
+		<h1>This lesson is about functions that cotain pramiters</h1>
+	</head>
+	<body>
+		<?php  
+			function simpleFunction($number1, $number2) {
+				$total = $number1 + $number2;
+
+				echo "sum of the two numbers is : $total";
+			}
+
+			
+
+
+		?>
+	</body>
+</html>
