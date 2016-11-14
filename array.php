ec54e6e140f3010bab5f25bb9fd24bf7f28c491a
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-13 21:50:54 -0800

    more on array

--- array.php
+++ array.php
@@ -1,6 +1,6 @@
 <?php 
 
 $cities = array("New York", "Chicago", "Miamy", "San Francisco");
-echo "I like ".$cities[3];
+echo "I like ".$cities[0] . " and " .$cities[1] ." and " . $cities[2] ;
 
 ?>
