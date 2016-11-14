3ff6fa74cbbe8099428174b02b40d240c571bf8e
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-13 21:58:55 -0800

    starting challenging with  a form

---
+++ challenge/1.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Challenge 1</title>
+	</head>
+	<body>
+		<form action="">
+			<h2>Please write your name</h2>
+			<input type="text" name="name">
+			<input type="submit" value="Submit Name">
+		</form>
+
+		
+	</body>
+</html>
