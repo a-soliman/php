ed3cdedf3bbc45ae6c83c83dc2f9e4f6a8bf3c14
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-13 22:03:44 -0800

    complated the form challenge

--- challenge/1.php
+++ challenge/1.php
@@ -3,12 +3,19 @@
 		<title>Challenge 1</title>
 	</head>
 	<body>
-		<form action="">
+		<form action="" method="POST">
 			<h2>Please write your name</h2>
 			<input type="text" name="name">
 			<input type="submit" value="Submit Name">
 		</form>
+		<!--Starting the PHP program-->
+		<?php 
+			
+			/*Retrivining the data*/
+			$name = $_POST['name'];
 
-		
+			/* Priniting the name*/
+			echo "<h3>Hello $name</h3";
+		?>
 	</body>
 </html>
